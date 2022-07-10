52b76867f3a00c069ef82451a62b67777dc699dc
ljh9 <dkssud2208@naver.com>
2022-07-11 00:38:06 +0900

    React useState, useTransition, useDeferredValue

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 /* eslint-disable */
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useDeferredValue, useState, useTransition } from 'react';
+
+let a = new Array(10).fill(0)
 
 function App() {
+  //useDeferredValue, useState, useTransition
+  let [name, setName] = useState('');
+  let [isPending, startTransition] = useTransition();
+  let state = useDeferredValue(name);
+  //
+
+
   let post = '강남 맛집';
   let [a, b] = useState(['맛집 추천1', '맛집 추천2', '맛집 추천3'])
   let [good, c] = useState(0);
@@ -19,6 +28,20 @@ function App() {
 
   return (
     <div className="App">
+      {/* useDeferredValue, useState, useTransition */}
+      <input onChange={(e =>{
+        startTransition(()=>{
+          setName(e.target.value)
+        })
+      })}/>
+      {
+        isPending ? 'Loading..' : 
+        a.map(()=>{
+          return <div>{state}</div>
+        })
+      }
+      {/*  */}
+
       <div className="black-nav">
         <h4 style={{color : 'red', fontSiez: '16px'}}>블로그임</h4>
       </div>
